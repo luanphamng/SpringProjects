2686b632696db1802dd40ae77522c352af7a5c98
Luan(Liam) N. Pham <Liam@enclave.com>
2020-01-06 17:23:14 +0700

    update frontend

--- chordzone/misc/script/2SongTable.sql
+++ chordzone/misc/script/2SongTable.sql
@@ -1,13 +1,12 @@
-CREATE TABLE `song` (
+CREATE TABLE IF NOT EXISTS `song` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   `artist` varchar(45) NOT NULL,
-  `lyric` varchar(5000) NOT NULL,
+  `lyric` text NOT NULL,
   `visitcount` int(11) DEFAULT NULL,
   `gam` varchar(15) DEFAULT NULL,
-  `singer` varchar(200) NOT NULL,
+  `singer` varchar(100) NOT NULL,
   `category` varchar(100) NOT NULL,
-  `postdate` datetime DEFAULT NULL,
+  `postdate` datetime NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-SELECT * FROM testchorddb.song;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
