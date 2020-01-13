c1a15b5c7bd0165e773b716ee50bbf26eee0a75c
Luan(Liam) N. Pham <Liam@enclave.com>
2020-01-13 17:17:27 +0700

    Get url and img done

--- Top10NewsTuoiTre/src/main/java/Application.java
+++ Top10NewsTuoiTre/src/main/java/Application.java
@@ -2,15 +2,16 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Map;
 
 public class Application {
 
     private HashSet<String> links;
-
     public Application() {
         links = new HashSet<String>();
     }
@@ -32,25 +33,28 @@ public class Application {
                 System.out.println("============= Link On Page =============");
                 System.out.println(linksOnPage);
                 System.out.println("============= End on Page =============");
-                Elements linksSeperates = linksOnPage.select("li");
-                System.out.println("============= Link seperate =============");
-                for(Element e : linksSeperates){
-                    System.out.println("Seperate: " + e);
+                Elements linksArticle = linksOnPage.select("li");
+                Elements linksImage = linksOnPage.select("img");
+
+                for (Element page : linksArticle) {
+                    if (page.childNodes().size() > 1) {
+                        for (Node node : page.childNodes()) {
+                            String nodeAttr = node.attr("href");
+                            if (nodeAttr != "") {
+                                System.out.println("https://tuoitre.vn" + nodeAttr);
+                                break;
+                            }
+                        }
+                    }
                 }
-//                System.out.println(linksSeperates);
-                System.out.println("============= End on link seperate =============");
-                //5. For each extracted URL... go back to Step 4.
-//                for (Element page : linksOnPage) {
-////                    getPageLinks(page.attr("abs:href"));
-////                }
-                for (Element page : linksSeperates) {
-                    getPageLinks(page.attr("abs:href"));
+
+                for (Element page : linksImage) {
+                    String nodeAttr = page.attr("src");
+                    if (nodeAttr != "") {
+                        System.out.println(nodeAttr);
+                    }
                 }
-//                for (Element headline : linksSeperates) {
-//                    String t = headline.attr("src");
-//                    System.out.print("Title: " + t);
-////                    System.out.println("   Url: " + headline.attr("href"));
-//                }
+
             } catch (IOException e) {
                 System.err.println("For '" + URL + "': " + e.getMessage());
             }
